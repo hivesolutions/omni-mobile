904e578b55f68a0ecfef935fa91be50c8ee5751f
João Magalhães <joamag@gmail.com>
2011-03-10 19:32:10 +0000

    new code added

--- omni_mobile/main.m
+++ omni_mobile/main.m
@@ -1,17 +1,48 @@
+// Hive Omni Erp
+// Copyright (C) 2008 Hive Solutions Lda.
 //
-//  main.m
-//  omni_mobile
+// This file is part of Hive Omni Erp.
 //
-//  Created by João Magalhães on 3/10/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+// Hive Omni Erp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
+// Hive Omni Erp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Hive Omni Erp. If not, see <http://www.gnu.org/licenses/>.
+
+// __author__    = João Magalhães <joamag@hive.pt>
+// __version__   = 1.0.0
+// __revision__  = $LastChangedRevision: 2390 $
+// __date__      = $LastChangedDate: 2009-04-02 08:36:50 +0100 (qui, 02 Abr 2009) $
+// __copyright__ = Copyright (c) 2008 Hive Solutions Lda.
+// __license__   = GNU General Public License (GPL), Version 3
 
 #import <UIKit/UIKit.h>
 
-int main(int argc, char *argv[])
-{
+/**
+ * Main entry point function to be called upon
+ * initialization.
+ *
+ * @param argc The number of arguments received.
+ * @param argv The argument values received.
+ * @return The application return code.
+ */
+int main(int argc, char *argv[]) {
+    // creates a new auto release pool
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    
+    // runs the ui application main and retrieves the return value
+    int returnValue = UIApplicationMain(argc, argv, nil, nil);
+    
+    // releases the (auto release) pool
     [pool release];
-    return retVal;
+    
+    // returns the return value
+    return returnValue;
 }
