862443b5b5986f1b92f08ab772b2f65c90ab23d5
João Magalhães <joamag@hive.pt>
2011-03-11 15:33:26 +0000

    new files added

---
+++ omni_mobile/classes/MenuViewController.h
@@ -0,0 +1,16 @@
+//
+//  MenuViewController.h
+//  omni_mobile
+//
+//  Created by João Magalhães on 3/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MenuViewController : UIViewController {
+    
+}
+
+@end
