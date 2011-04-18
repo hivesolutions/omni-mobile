73b52ac2671d9da42c8e215e33f97cc3b439c695
João Magalhães <joamag@hive.pt>
2011-04-18 17:57:07 +0100

    initial image support

--- omni_mobile/classes/UserViewController.m
+++ omni_mobile/classes/UserViewController.m
@@ -70,6 +70,8 @@
     NSString *secretAnswer = AVOID_NULL([remoteData objectForKey:@"secret_answer_hash"]);
     NSDictionary *person = AVOID_NULL_DICTIONARY([remoteData objectForKey:@"person"]);
     NSString *personName = AVOID_NULL([person objectForKey:@"name"]);
+    NSDictionary *primaryMedia = AVOID_NULL_DICTIONARY([remoteData objectForKey:@"primary_media"]);
+    NSString *base64Data = AVOID_NULL([primaryMedia objectForKey:@"base_64_data"]);
 
     // checks if the persons is available
     BOOL isPersonAvailable = [person count] == 0 ? NO : YES;
@@ -85,6 +87,7 @@
     // creates the image item
     HMItem *imageItem = [[HMItem alloc] initWithIdentifier:@"image"];
     imageItem.description = @"person_header.png";
+    imageItem.data = [HMBase64Util decodeBase64WithString:base64Data];
 
     // creates the menu header group
     HMNamedItemGroup *menuHeaderGroup = [[HMNamedItemGroup alloc] initWithIdentifier:@"menu_header"];
